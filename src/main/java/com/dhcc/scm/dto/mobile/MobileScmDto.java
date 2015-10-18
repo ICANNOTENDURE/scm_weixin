e1f764042b68ff336424ef3cc67057041e6de6fd
zhouxin <zhouxin@dhcc.com>
2015-10-18 23:03:08 +0800

    1

---
+++ src/main/java/com/dhcc/scm/dto/mobile/MobileScmDto.java
@@ -0,0 +1,16 @@
+/**
+ * Dto 
+ * template by zxx
+ */
+package com.dhcc.scm.dto.mobile;
+
+import com.dhcc.framework.transmission.dto.BaseDto;
+
+public class MobileScmDto extends BaseDto {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
