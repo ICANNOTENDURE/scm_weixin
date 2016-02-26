25a46f69c2bec1d4a9bffd6e150da08e24f004a3
zhouxin <zhouxin@dhcc.com>
2016-02-26 18:04:24 +0800

    6

---
+++ src/main/java/com/dhcc/scm/blh/weixin/MpMessageBlh.java
@@ -0,0 +1,87 @@
+/**
+ * 通过模板生成Blh 
+ * template by zxx
+ */
+package com.dhcc.scm.blh.weixin;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.WxMpCustomMessage;
+
+import org.springframework.stereotype.Component;
+
+import com.dhcc.framework.app.blh.AbstractBaseBlh;
+import com.dhcc.framework.app.service.CommonService;
+import com.dhcc.framework.common.config.PropertiesBean;
+import com.dhcc.scm.entity.hop.HopCtloc;
+import com.dhcc.scm.entity.hop.HopCtlocDestination;
+import com.dhcc.scm.entity.hop.Hospital;
+import com.dhcc.scm.entity.ord.OrderDetail;
+import com.dhcc.scm.entity.userManage.NormalAccount;
+import com.dhcc.scm.entity.userManage.NormalUser;
+import com.dhcc.scm.entity.weixin.MpUser;
+
+@Component
+public class MpMessageBlh extends AbstractBaseBlh {
+
+	@Resource
+	private CommonService commonService;
+
+	@Resource
+	private WxMpService wxMpService;
+
+	public MpMessageBlh() {
+
+	}
+
+	public void sendMessByOrd(OrderDetail orderDetail) {
+		try {
+			String host = PropertiesBean.getInstance().getProperty("config.sci.dhs");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+
+			Long vendorId = orderDetail.getOrderVenId();
+			List<NormalUser> normalUsers=commonService.findByProperty(NormalUser.class, "vendorId", vendorId);
+			if(normalUsers.size()==0){
+				return;
+			}
+			StringBuffer sb = new StringBuffer();
+			Hospital hospital = commonService.get(Hospital.class, orderDetail.getOrderHopId());
+			sb.append(hospital.getHospitalName());
+			if (orderDetail.getOrderPurLoc() != null) {
+				HopCtloc hopCtloc = commonService.get(HopCtloc.class, orderDetail.getOrderPurLoc());
+				sb.append("_" + hopCtloc.getName());
+			}
+			sb.append("给您的新订单" + System.getProperty("line.separator") + System.getProperty("line.separator"));
+			sb.append("订单时间:" + sdf.format(orderDetail.getOrderDate()) + System.getProperty("line.separator"));
+
+			if (orderDetail.getOrderRecDestination() != null) {
+				HopCtlocDestination ctlocDestination = commonService.get(HopCtlocDestination.class, orderDetail.getOrderRecDestination());
+				sb.append("收货地址:" + ctlocDestination.getDestination() + System.getProperty("line.separator"));
+			}
+			sb.append("订单单号:" + orderDetail.getOrderNo() + System.getProperty("line.separator") + System.getProperty("line.separator"));
+			sb.append("请您及时处理订单,谢谢");
+
+			WxMpCustomMessage.WxArticle article1 = new WxMpCustomMessage.WxArticle();
+			article1.setUrl(host + "weixin/wxMessageCtrl!mbListOrderDetail.htm?dto.orderDetail.orderNo=" + orderDetail.getOrderNo());
+			article1.setDescription(sb.toString());
+			article1.setTitle("新订单提醒");
+			
+			for(NormalUser normalUser:normalUsers){
+				String[] propertyNames={"wxMpSciPointer","wxMpSend"};
+				Object[] values={normalUser.getUserId(),"1"};
+				List<MpUser> mpUsers=commonService.findByProperties(MpUser.class, propertyNames, values);
+				for(MpUser mpUser:mpUsers){
+					WxMpCustomMessage.NEWS().toUser(mpUser.getWxMpOpenId()).addArticle(article1).build();
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/com/dhcc/scm/web/weixin/action/MpMessageAction.java
@@ -0,0 +1,63 @@
+/**
+ * 通过模板生成Action 
+ * template by zxx
+ */
+package com.dhcc.scm.web.weixin.action;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.context.annotation.Scope;
+
+import com.dhcc.framework.annotation.Blh;
+import com.dhcc.framework.annotation.JResult;
+import com.dhcc.framework.annotation.JsonResults;
+import com.dhcc.framework.exception.BaseException;
+import com.dhcc.framework.transmission.dto.BaseDto;
+import com.dhcc.framework.transmission.event.BusinessRequest;
+import com.dhcc.framework.web.BaseAction;
+import com.dhcc.scm.dto.weixin.MpUserDto;
+
+@Namespace(value = "/weixin")
+@Scope("prototype")
+@Action(value = "mpMessageCtrl", results = {
+		@Result(name = "list", location = "/WEB-INF/jsp/weixin/MpUser.jsp"),
+		@Result(name = "listMain", location = "/WEB-INF/jsp/weixin/MpUser.jsp")})
+@Blh("mpUserBlh")
+@JsonResults({@JResult(BlhMethod="findById",ognlExpress="dto.mpUser")})
+public class MpMessageAction extends BaseAction {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private MpUserDto dto = new MpUserDto();
+	
+	@Override
+	public String directlyJump() {
+		//直接返回jsp
+		if("listMain".equals(super.getBusinessFlow())){
+			return "listMain";
+		}
+		return null;
+	}
+	
+	@Override
+	public BaseDto getBaseDto() {
+
+		return dto;
+	}
+	
+	@Override
+	protected void prepareRequest(BusinessRequest reqEvent) throws BaseException {
+
+		reqEvent.setDto(dto);
+	}
+	
+	public MpUserDto getDto() {
+		return dto ;
+	}
+	
+	public void setDto(MpUserDto dto) {
+		this.dto = dto;
+	}
+
+}
